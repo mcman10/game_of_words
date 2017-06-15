4d0dad74e7113ab66e4e7fd8cd185418d15d45cc
Timosha Vlaskov <vlaskov42@gmail.com>
2017-06-15 17:41:29 +0300

    Adding time frame & label

--- mainwindow.py
+++ mainwindow.py
@@ -23,20 +23,14 @@ class MainWindow (gtk.Window):
             #color = gtk.gdk.color_parse(BG_COLOR)
             #self.modify_bg(gtk.STATE_NORMAL, color)
 
+            # RESIZE MAINWINDOW
             w,h = gtk.gdk.screen_width(), gtk.gdk.screen_height() 
             self.resize(w/2,h/2)
 
-            letters_alignment = gtk.Alignment(xalign=0, yalign=0.5)
+            #TABLE OF ENTRIES WIRH LETTERS
             table = gtk.Table(rows=20, columns=7, homogeneous=True)
+            letters_alignment = gtk.Alignment(xalign=0, yalign=0.5)
             letters_alignment.add(table)
-
-            buttons_alignment = gtk.Alignment(xalign =1, yalign=0)
-
-            main_hbox = gtk.HBox(homogeneous=False, spacing=0);
-
-            main_hbox.add(letters_alignment)
-            main_hbox.add(buttons_alignment)
-            self.add(main_hbox)
 
             current_length = 3
             change_length_where_row = 3
@@ -52,6 +46,36 @@ class MainWindow (gtk.Window):
                         change_length_where_row += current_length
                         current_length +=1
 
+            #INFORMATION LABELS
+            self.time_label = gtk.Label("00:00:00")
+            time_frame = gtk.Frame(label="time")
+            time_frame.add(self.time_label)
+            labels_vbox = gtk.VBox(homogeneous=False, spacing=5)
+            labels_vbox.add(time_frame)
+            labels_alignment = gtk.Alignment(xalign=0.5, yalign=0.05, xscale = 0.95)
+            labels_alignment.add(labels_vbox)
+
+            #CONTROL BUTTONS
+            start_button = gtk.Button(label="start")
+            start_button.connect("clicked", self.start_button_clicked)
+            check_button = gtk.Button(label="check")
+            check_button.connect("clicked", self.check_button_clicked)
+            button_vbox = gtk.VBox(homogeneous=True, spacing=5)
+            button_vbox.add(start_button)
+            button_vbox.add(check_button)
+
+            buttons_alignment = gtk.Alignment(xalign =0.9, yalign=0.05, xscale=0.95)
+            buttons_alignment.add(button_vbox)
+
+            main_hbox = gtk.HBox(homogeneous=False, spacing=0);
+
+            main_hbox.add(letters_alignment)
+            main_hbox.add(labels_alignment)
+            main_hbox.add(buttons_alignment)
+            self.add(main_hbox)
+
+            
+
         def set_letters(self, d):
             row, column = 0,0 
             for i in range (3,8):
@@ -62,5 +86,8 @@ class MainWindow (gtk.Window):
                     column = 0
                     row+=1
 
-	def start_button_click(self):
-            pass
+	def start_button_clicked(self, widget, data=None):
+            print "Start button clicked!!!"
+
+	def check_button_clicked(self, widget, data=None):
+            print "Check button clicked!!!"
