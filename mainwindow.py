7247d6038836f667c91582680e5f8e51fe65bf59
Timosha Vlaskov <vlaskov42@gmail.com>
2017-06-16 16:48:38 +0300

    Adding a stopwatch to time label

--- mainwindow.py
+++ mainwindow.py
@@ -3,9 +3,39 @@
 import pygtk
 pygtk.require("2.0")
 import gtk
-import operator
+import datetime
+from twisted.internet.task import LoopingCall
+from random import randint
 
 #BG_COLOR="#0b0bb0"	
+
+def print_table(d):
+	for i in range(3,8):
+		for j in range(1,i):
+			print d[i*10+j]
+
+def gen_table(syll):
+	"""generate a game table as dict d {31:'??.',32:'.??',41:'??..',42:'.??.',...} """
+	d = {31:'...',32:'...',41:'....',42:'....',43:'....',51:'.....',52:'.....',53:'.....',54:'.....',
+			61:'......',62:'......',63:'......',64:'......',65:'......',
+			71:'.......',72:'.......',73:'.......',74:'.......',75:'.......',76:'.......'}
+	for i in range(3,8):
+		for j in range(1,i):
+			w=list(d[10*i+j])
+			w[j-1]=syll[0]
+			w[j]=syll[1]
+			d[10*i+j]="".join(w)
+	return d
+
+def rand_syll():
+	""" generate a random russian syllable """
+	vovels={1:'а',2:'е',3:'ё',4:'и',5:'о',6:'у',7:'ы',8:'э',9:'ю',10:'я'}
+	conconants={1:'б',2:'в',3:'г',4:'д',5:'ж',6:'з',7:'к',8:'л',9:'м',10:'н',
+			11:'п',12:'р',13:'с',14:'т',15:'ф',16:'х',17:'ч',18:'ш',19:'щ'}
+	i=randint(1,10)
+	j=randint(1,19)
+	syllable=(conconants[j],vovels[i])
+	return syllable 
 
 class MainWindow (gtk.Window):
 	def __init__(self):
@@ -56,13 +86,14 @@ class MainWindow (gtk.Window):
             labels_alignment.add(labels_vbox)
 
             #CONTROL BUTTONS
-            start_button = gtk.Button(label="start")
-            start_button.connect("clicked", self.start_button_clicked)
-            check_button = gtk.Button(label="check")
-            check_button.connect("clicked", self.check_button_clicked)
+            self.start_button = gtk.Button(label="start")
+            self.check_button = gtk.Button(label="check")
+            self.check_button.connect("clicked", self.check_button_clicked)
+            self.check_button.set_sensitive(False)
+            self.start_button.connect("clicked", self.start_button_clicked)
             button_vbox = gtk.VBox(homogeneous=True, spacing=5)
-            button_vbox.add(start_button)
-            button_vbox.add(check_button)
+            button_vbox.add(self.start_button)
+            button_vbox.add(self.check_button)
 
             buttons_alignment = gtk.Alignment(xalign =0.9, yalign=0.05, xscale=0.95)
             buttons_alignment.add(button_vbox)
@@ -87,7 +118,20 @@ class MainWindow (gtk.Window):
                     row+=1
 
 	def start_button_clicked(self, widget, data=None):
-            print "Start button clicked!!!"
+            syll = rand_syll()
+            d=gen_table(syll)
+            self.set_letters(d)
+            self.start_time = datetime.datetime.now()
+            self.lc = LoopingCall(self.tired_task)
+            self.lc.start(1)
+            self.start_button.set_sensitive(False)
+            self.check_button.set_sensitive(True)
+
+        def tired_task(self):
+            self.time_label.set_text(str(datetime.datetime.now()-self.start_time))
 
 	def check_button_clicked(self, widget, data=None):
-            print "Check button clicked!!!"
+            self.lc.stop()
+            self.time_label.set_text(str(datetime.datetime.now()-self.start_time))
+            self.start_button.set_sensitive(True)
+            self.check_button.set_sensitive(False)
---
+++ network.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""game of words network check of words"""
+# http://orfo.ruslang.ru/search/word    
